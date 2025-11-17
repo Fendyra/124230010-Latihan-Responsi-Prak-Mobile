df8f4a36c4856aba340672a732cfb8303c556ffa
fendyra <fendyrarestu2004@gmail.com>
2025-11-18 02:22:46 +0700

    Update Detail

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ void main() {
   testWidgets('App starts at login page when not logged in', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp(isLoggedIn: false));
 
-    expect(find.text('Selamat Datang di Otsu'), findsOneWidget);
+    expect(find.text('Welcome to Otsu'), findsOneWidget);
     expect(find.text('Login'), findsWidgets);
     expect(find.text('Register'), findsOneWidget);
   });
